36ee7971b34a099ec300d8117cbda30cd2d6a26b
jamesxsc <james@xsc.co.uk>
2024-11-23 22:41:07 +0000

    Initial Commit

---
+++ intercom/intercomApp.swift
@@ -0,0 +1,17 @@
+//
+//  intercomApp.swift
+//  intercom
+//
+//  Created by James Conway on 23/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct intercomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
